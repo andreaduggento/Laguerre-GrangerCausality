25957ca57a3990b365cbe6d02c1571242bb460dc
Andrea Duggento <andrea@calavera>
2019-05-26 21:49:30 +0200

    Core functions

--- LaguerreGC_GC.m
+++ LaguerreGC_GC.m
@@ -1,4 +1,4 @@
-function [ GC , llsdata , N , L ] =  LaguerreGC_rightdrop(data,alpha,orders)
+function [ AA , GC , llsdata , N , L ] =  LaguerreGC_rightdrop(data,alpha,orders)
 
 	[ N , L ] = size(data);
 	if (N<L); data=data'; end;
@@ -7,9 +7,18 @@ function [ GC , llsdata , N , L ] =  LaguerreGC_rightdrop(data,alpha,orders)
 	GC = NaN(L,L);
 
 	rightdrop = LaguerreGC_rightdrop(alpha,orders);
+	maxdrop = max(rightdrop);
 	sdata = normalize(data,1);
 	llsdata = LaguerreGC_convolve(sdata,alpha,orders);
 
+%	i->j
+
+	predictees=sdata(maxdrop+1:end,:);
+	obsMatrix=llsdata(maxdrop:end-1,:,:);
+
+%	size(obsMatrix)
+
+	[ globalcovD globalAA ] = LaguerreGC_lfit(predictees,obsMatrix);
 
 
 
--- LaguerreGC_lfit.m
+++ LaguerreGC_lfit.m
@@ -1,3 +1,10 @@
-function  LaguerreGC_lfit()
-
+function  [ covD AA ] = LaguerreGC_lfit(predictees,obs)
+	[ N L O ] = size(obs);
+	reObs=reshape(obs,N,L*O);
+	size(reObs)
+	size(predictees)
+	AA = reObs\predictees;
+    	predictions=reObs*AA; 
+	res=predictions-predictees;
+	covD=cov(res);
 end
